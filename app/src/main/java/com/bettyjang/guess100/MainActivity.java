53271f5c96df36e6d4e7967c83b1a9701dfe107a
Betty Jang <bettyjang1005@gmail.com>
2017-11-14 10:48:07 +0800

    Guess

--- app/src/main/java/com/bettyjang/guess100/MainActivity.java
+++ app/src/main/java/com/bettyjang/guess100/MainActivity.java
@@ -1,10 +1,14 @@
 package com.bettyjang.guess100;
 
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,6 +17,9 @@ public class MainActivity extends AppCompatActivity {
     private View edInfo;
     private View edSecret;
 
+    int ran = new  Random().nextInt(100)+1;
+    int max = 100;
+    int min = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,5 +33,22 @@ public class MainActivity extends AppCompatActivity {
         edInfo = findViewById(R.id.info);
         edNumber = (EditText) findViewById(R.id.number);
         Button send = (Button) findViewById(R.id.send);
+    }
+    public void Guess100 (View view){
+       /* int ran = new Random().nextInt(100)+1;
+        int max = 100;
+        int min = 1;*/
+        TextView rand = (TextView) findViewById(R.id.secret);
+        rand.setText(Integer.toString(ran));
+        EditText edNumber = (EditText) findViewById(R.id.number);
+        int number = Integer.parseInt(edNumber.getText().toString());
+        if (number < ran){
+            new AlertDialog.Builder(this).setMessage(number+"to"+max).show();
+        }else if (number > ran){
+            new AlertDialog.Builder(this).setMessage(min+"to"+number).show();
+        }else {
+            new AlertDialog.Builder(this).setMessage("恭喜答對").show();
+
+        }
     }
 }
